db447201020c611752c9433cad2a48b3f275bea8
vassilijoffroy <vassili.joffroy@rubrash.com>
2023-10-14 13:20:53 +0200

    fix: add custom url to access to BO

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('admin')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
