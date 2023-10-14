49f61f755a0d1fe8a4e617b2142a4ec8dc8622d9
vassilijoffroy <vassili.joffroy@rubrash.com>
2023-10-14 13:09:42 +0200

    fix: remove fortify

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\SocialAuthController;
 use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
