8db86c174eae641e95698038b9a7cfeaedb21f55
Galois <Galoisb95@gmail.com>
2023-11-09 18:51:48 +0100

    Remove unused code

--- resources/views/stripe/index.blade.php
+++
@@ -1,8 +0,0 @@
-<button id="checkout-button"
-        data-stripe-key="{{ config('stripe.key') }}"
-        data-session-id="{{ $checkout_session_id }}">
-    Payer
-</button>
-
-<script src="https://js.stripe.com/v3/"></script>
-<script src="{{asset('js/stripe/stripe.js')}}"></script>
