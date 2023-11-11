810f373f3ac25b25cfc5acc8f51046911cae49f9
Galois <Galoisb95@gmail.com>
2023-11-11 18:56:52 +0100

    display existing event in ClientBookTeacherCalendar

--- app/Filament/Widgets/ClientBookTeacherCalendar.php
+++ app/Filament/Widgets/ClientBookTeacherCalendar.php
@@ -2,9 +2,24 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Event;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
 
 class ClientBookTeacherCalendar extends FullCalendarWidget
 {
-
+    public function fetchEvents(array $fetchInfo): array
+    {
+        return Event::query()
+            ->where('starts_at', '>=', $fetchInfo['start'])
+            ->where('ends_at', '<=', $fetchInfo['end'])
+            ->get()
+            ->map(
+                fn (Event $event) => [
+                    'title' => $event->name,
+                    'start' => $event->starts_at,
+                    'end' => $event->ends_at,
+                ]
+            )
+            ->all();
+    }
 }
