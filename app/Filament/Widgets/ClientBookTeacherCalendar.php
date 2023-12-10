ba4ea5d63b9cce4d620283ce5b25aa141212348c
Galois <Galoisb95@gmail.com>
2023-12-10 01:10:06 +0100

    add custom book form

--- app/Filament/Widgets/ClientBookTeacherCalendar.php
+++ app/Filament/Widgets/ClientBookTeacherCalendar.php
@@ -4,8 +4,11 @@ namespace App\Filament\Widgets;
 
 use App\Models\Event;
 use Filament\Actions\CreateAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Model;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
@@ -40,25 +43,56 @@ class ClientBookTeacherCalendar extends FullCalendarWidget
         return CreateAction::make()
             ->form([
                 Section::make('Récapitulatif')
+                    ->columns()
+                    ->icon('heroicon-m-newspaper')
                     ->schema([
-                        // NOTE(BENJ): Recapitulatif du caoch
-                        // Recapitulatif de ses sports etc..
-                        // Recap prix (50e en dur)
-
                         TextInput::make('title')
+                            ->label(__('common.book.title'))
                             ->default($this->record->title)
+                            ->disabled()
                             ->readOnly(),
+                        TextInput::make('price')
+                            ->label(__('common.book.price'))
+                            ->default('50 €')
+                            ->disabled()
+                            ->readOnly(),
+                        DateTimePicker::make('Heure de début')
+                            ->label(__('common.book.startHour'))
+                            ->disabled()
+                            ->default($this->record->start),
+                        DateTimePicker::make('Heure de fin')
+                            ->label(__('common.book.endHour'))
+                            ->disabled()
+                            ->default($this->record->end),
+                        Textarea::make('description')
+                            ->label(__('common.book.coachDetails'))
+                            ->default($this->teacher->description)
+                            ->disabled()
+                            ->readOnly()
+                            ->autosize()
+                            ->columnSpanFull(),
+                        Textarea::make('coach-specialities')
+                            ->label(__('common.book.coachSpecialities'))
+                            ->default(implode(' | ', $this->teacher->specialities()->pluck('name')->toArray()))
+                            ->disabled()
+                            ->autosize()
+                            ->readOnly()
+                            ->columnSpanFull(),
                     ]),
-
-                Select::make('activity') // TODO Rename
+                Select::make('speciality')
+                    ->label(__('common.speciality'))
+                    ->placeholder(__('common.book.chooseSpeciality'))
                     ->options($this->teacher->specialities()->pluck('name', 'id'))
+                    ->columnSpanFull()
+                    ->required()
                     ->searchable(),
-
-                // TODO add message to the coach
-                // TODO: add any fields u judge required
-                // https://filamentphp.com/docs/3.x/actions/prebuilt-actions/create
-                // Voir les notifs
+                Textarea::make('message')
+                    ->placeholder(__('common.book.messageForCoach'))
+                    ->autosize()
+                    ->columnSpanFull(),
             ])
+            ->successNotificationTitle(__('common.book.bookSucceeded'))
+            ->failureNotificationTitle(__('common.book.bookFailed'))
             ->createAnother(false);
     }
 }
--- lang/fr/common.php
+++ lang/fr/common.php
@@ -19,6 +19,18 @@ return [
     'createdAt' => 'Crée le',
     'deletedAt' => 'Supprimé le',
     'updatedAt' => 'Mis à jour le',
-    'description' => 'Déscription',
-    'verifiedAt' => 'Vérifié le'
+
+    'book' => [
+        'startHour' => 'Heure de début',
+        'endHour' => 'Heure de fin',
+        'coachDetails' => 'Détails du coach',
+        'verifiedAt' => 'Vérifié le',
+        'price' => 'Prix',
+        'title' => 'Titre',
+        'coachSpecialities' => 'Spécialités du coach',
+        'chooseSpeciality' => 'Choisissez une spécialité',
+        'messageForCoach' => 'Vous pouvez indiquer ici toutes les informations que vous voulez transmettre à votre coach',
+        'bookSucceeded' => 'La réservation à été effectué',
+        'bookFailed' => 'La réservation à échoué',
+    ],
 ];
