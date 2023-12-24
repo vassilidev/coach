a59b4849f03d8ae041dcf39aec378be26b85e1b8
vassilijoffroy <vassili.joffroy@rubrash.com>
2023-12-24 12:10:55 +0100

    fix: seeder for prod

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,10 +12,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        (User::factory()->create([
+        (User::create([
             'name'              => 'Admin',
             'email'             => 'admin@admin.com',
-            'email_verified_at' => now(),
+            'email_verified_at'  => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]))->assignRole('Super Admin');
 
