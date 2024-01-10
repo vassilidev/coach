6048262c1b824bce6584924f5a6e47a02c1d6d5d
vassilijoffroy <vassili.joffroy@rubrash.com>
2024-01-10 15:31:32 +0100

    fix: migration

--- database/migrations/2023_11_09_181655_create_events_table.php
+++ database/migrations/2023_11_09_181655_create_events_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Teacher;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('events', static function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Teacher::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('title')->nullable();
             $table->dateTime('start');
             $table->dateTime('end');
